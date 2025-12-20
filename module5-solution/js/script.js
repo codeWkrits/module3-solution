b6f5cc702b769c61447869901f542c8c9cadcdaa
codeWkrits <krits2619@gmail.com>
2025-12-20 22:05:16 +0530

    completed module 5 assignment

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -65,7 +65,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 // TODO: STEP 0: Look over the code from
 // *** start ***
-// to
+$ajaxUtils.sendGetRequest(
+  allCategoriesUrl,
+  buildAndShowHomeHTML,
+  true
+);
+
 // *** finish ***
 // below.
 // We changed this code to retrieve all categories from the server instead of
@@ -92,6 +97,8 @@ $ajaxUtils.sendGetRequest(
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
+  var randomCategory = chooseRandomCategory(categories);
+
 
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
@@ -102,11 +109,21 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
+      $ajaxUtils.sendGetRequest(
+  homeHtmlUrl,
+  function (homeHtml) {
+    buildAndShowHomeHTMLWithCategory(homeHtml, randomCategory);
+  },
+  false
+  );
+
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
+      var randomCategoryShortName = randomCategory.short_name;
+
       // WARNING! You are inserting something that will have to result in a valid Javascript
       // syntax because the substitution of {{randomCategoryShortName}} becomes an argument
       // being passed into the $dc.loadMenuItems function. Think about what that argument needs
@@ -122,6 +139,14 @@ function buildAndShowHomeHTML (categories) {
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
+      homeHtml = insertProperty(
+  homeHtml,
+  "randomCategoryShortName",
+  randomCategoryShortName
+  );
+
+  document.querySelector("#main-content").innerHTML = homeHtml;
+
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
